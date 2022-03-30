51c6cdcee4694f801aea589d2dbbbb6204e8c460
fitriariskaa19 <72713210+fitriariskaa19@users.noreply.github.com>
2022-03-31 05:40:29 +0700

    Add files via upload

---
+++ nomer 5.py
@@ -0,0 +1,17 @@
+# Fitri Ariska
+# 20051397082
+# D4 MI 2020B
+
+class Stack(list):
+    push = list.append
+
+def modify_stack(symbolString):
+    stack=Stack()
+    result = []
+    for character in symbolString:
+        if character != '*':
+            stack.push(character)
+        else:
+            result.append(stack.pop())
+    return ''.join(result)
+print(modify_stack('EAS*Y*QUE***ST***IO*N***'))
